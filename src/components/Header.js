202cf681d14811508a74484e8c885a63ffdfe5fe
shivamjain1411 <realshivamjain@gmail.com>
2024-08-12 22:30:55 +0530

    naviagtion completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,10 @@ const Header = () => {
       <div className="flex justify-between ">
         <div className=" flex flex-col justify-start items-center">
           <div className="flex">
-            <h1 className="text-6xl bg-blue-900 font-bold px-1 mx-1 ">Sumit</h1>
-            <h1 className="text-6xl text-blue-900 font-bold">Yadav</h1>
+            <h1 className="text-6xl bg-[#301c74] font-bold px-1 mx-1 ">
+              Sumit
+            </h1>
+            <h1 className="text-6xl text-[#301c74] font-bold">Yadav</h1>
           </div>
           <div className="">
             <h1 className="text-black font-bold">
@@ -17,13 +19,13 @@ const Header = () => {
         </div>
 
         <div className="flex justify-start space-x-4  items-center">
-          <h1 className="text-blue-950 font-extrabold">
+          <h1 className="text-[#301c74] font-extrabold">
             <div className="flex">
               <div>📞</div>
               8851505094
             </div>
           </h1>
-          <h1 className="text-blue-950 font-extrabold">
+          <h1 className="text-[#301c74] font-extrabold">
             <div className="flex">
               <div>✉️</div> Welcome@casumityadav.com
             </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 const Navigation = () => {
-  const menuItems = ["Home", "Services", "About", "Contact", "Resources"];
+  const menuItems = [
+    "Home",
+    "Book Consultancy",
+    "Income Tax",
+    "GST",
+    "Business Registration",
+    "Legal Service",
+    "Annual Service",
+    "Resources",
+  ];
 
   return (
-    <nav className="bg-indigo-700 text-white">
-      <ul className="container mx-auto flex justify-center space-x-6 py-3">
+    <nav className="bg-[#301c74] font-bold text-white">
+      <ul className="container mx-auto flex justify-center  space-x-16 py-2">
         {menuItems.map((item) => (
           <li key={item}>
             <a href="#" className="hover:text-yellow-300">
