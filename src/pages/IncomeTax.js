edfeb089db61b45ec7fef418deb61d6909ba5927
shivamjain1411 <realshivamjain@gmail.com>
2024-08-14 22:37:12 +0530

    Income tax 70%

--- src/pages/IncomeTax.js
+++ src/pages/IncomeTax.js
@@ -1,7 +1,26 @@
 import React from "react";
+import IncomeTaxReturn from "../components/IncomeTaxx/IncomeTaxReturn";
+import DocsReq from "../components/IncomeTaxx/DocsReq";
+import Process from "../components/IncomeTaxx/Process";
+import InfoCards from "../components/IncomeTaxx/InfoCards";
+import Types from "../components/IncomeTaxx/Types";
+import TaxTable from "../components/IncomeTaxx/TaxTable";
+import Peanulties from "../components/IncomeTaxx/Peanulties";
 
 function IncomeTax() {
-  return <div>IncomeTax</div>;
+  return (
+    <div>
+      <IncomeTaxReturn />
+      <DocsReq />
+      <div className="flex  justify-center">
+        <Process />
+      </div>
+      <InfoCards />
+      <Types />
+      <TaxTable />
+      <Peanulties />
+    </div>
+  );
 }
 
 export default IncomeTax;
